cc17fd71ec03451e7f2f43b96961411c24efe8ad
shindepriti <pritishinde34@gmail.com>
2020-04-15 20:50:30 +0530

    ADDED:TDD To Return Minimum Fare If Distance And Time Minimum

--- src/main/java/cabinvoice/InvoiceService.java
+++ src/main/java/cabinvoice/InvoiceService.java
@@ -1,10 +1,15 @@
 package cabinvoice;
 
 public class InvoiceService {
+
     double COST_PER_KILOMETER = 10;
     int PER_MINUTE_TIME = 1;
+    double MINIMUM_FARE = 5;
 
     public double getTotalFare(double distance, int time) {
-        return COST_PER_KILOMETER*distance+PER_MINUTE_TIME*time;
+        double totalFare = COST_PER_KILOMETER*distance+PER_MINUTE_TIME*time;
+        if (totalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return totalFare;
     }
 }
--- src/test/java/cabinvoice/InvoiceServiceTest.java
+++ src/test/java/cabinvoice/InvoiceServiceTest.java
@@ -1,16 +1,31 @@
 package cabinvoice;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
+    InvoiceService invoiceService;
+
+    @Before
+    public void initialize() {
+    invoiceService = new InvoiceService();
+    }
+
     @Test
     public void givenDistanceAndTime_shouldReturnTotalFare() {
-        InvoiceService invoiceService = new InvoiceService();
         double distance = 2.0;
         int time = 2;
         double fare = invoiceService.getTotalFare(distance,time);
         Assert.assertEquals(22,fare,0.0);
     }
 
+    @Test
+    public void givenLessDistanceAndTime_shouldReturnMinimumFare() {
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceService.getTotalFare(distance,time);
+        Assert.assertEquals(5.0,fare,0.0);
+
+    }
 }
